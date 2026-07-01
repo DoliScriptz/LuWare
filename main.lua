26ce8e09fef9d3b68457fa2e79d10597bf3716ab
Xynnn <dolitigaduasatu@gmail.com>
2026-07-01 22:10:24 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,20 @@
 print("@l.u.a.u on discord.")
 
 if game.PlaceId == 142823291 then
-  print("Loading: Murder Mystery 2.)
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/DoliScriptz/LuWare/games/mm2.lua"))()
- else
-    task.spawn(function()while wait()do pcall(function()local g=game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ErrorPrompt;g.TitleFrame.ErrorTitle.Text="LuWare";g.MessageArea.ErrorFrame.ErrorMessage.Text=r end)end end)game:GetService("Players").LocalPlayer:Kick(("Unsupported Game."))
-end      
+    print("Loading: Murder Mystery 2.")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DoliScriptz/LuWare/games/mm2.lua"))()
+else
+    local r = "Unsupported Game."
+
+    task.spawn(function()
+        while task.wait() do
+            pcall(function()
+                local g = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ErrorPrompt
+                g.TitleFrame.ErrorTitle.Text = "LuWare"
+                g.MessageArea.ErrorFrame.ErrorMessage.Text = r
+            end)
+        end
+    end)
+
+    game:GetService("Players").LocalPlayer:Kick(r)
+end
